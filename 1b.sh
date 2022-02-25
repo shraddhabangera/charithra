4372f49c68d55adbb8180788987b7ae5efd2f860
charithra3101 <98947213+charithra3101@users.noreply.github.com>
2022-02-25 07:33:26 -0800

    Update 1b.sh

--- 1b.sh
+++ 1b.sh
@@ -1,3 +1,5 @@
+/*1b. Write a shell script that accepts a path name and creates all the components in that path
+name as directories. For example, if the script is named as mpc, then the command mpc a/b/c/d */
 if [ $# -ne 0 ]
 then
 p=`echo $1|tr "/" " "`
